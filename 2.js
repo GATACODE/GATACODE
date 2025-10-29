59437bfe60f84b47e597ea76335f36a8d57d5e51
Elena Keranen <150839861+GATACODE@users.noreply.github.com>
2025-10-29 21:10:39 +0100

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,16 @@
+let i = 0;
+let sum = 0;
+while (i <= 10) {
+    console.log(i);
+    i++;
+    sum += i;
+}
+console.log(sum)
+
+
+
+for (let x = 1; x <= 10; x++) {
+    console.log(x);
+    sum += x;
+
+}
